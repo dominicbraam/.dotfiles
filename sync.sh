2251a6d1e01df9698931e1111e23c07fdfbbd89a
dominicbraam <dominic.braam@gmail.com>
2021-03-20 16:54:13 -0400

    initial commit

--- sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-rsync -avu --delete "$HOME/.config" "$HOME/.dotfiles"
-rsync -avu --delete "$HOME/Programs/dwm" "$HOME/.dotfiles"
-rsync -avu --delete "$HOME/Programs/dmenu" "$HOME/.dotfiles"
-rsync -avu --delete "$HOME/Programs/dwmbar" "$HOME/.dotfiles"
-rsync -avu --delete "$HOME/.gtkrc-2.0" "$HOME/.dotfiles"
-rsync -avu --delete "$HOME/.xinitrc" "$HOME/.dotfiles"
-rsync -avu --delete "$HOME/.zshenv" "$HOME/.dotfiles"

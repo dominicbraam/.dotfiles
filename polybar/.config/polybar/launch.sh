0082a49131a2e2e1d405d82150d9ed3373ca9c7a
dominicbraam <dominic.braam@gmail.com>
2022-01-19 11:19:46 -0400

    added -r flag: reloads bars after change to config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,7 +4,7 @@ killall -q polybar && sleep 2
 
 echo "---" | tee -a /tmp/polybar1.log
 
-polybar dominicbar 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar dominicbar-secondary 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar -r dominicbar 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar -r dominicbar-secondary 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."

5c215516d156cb04ecb9b4c5fc2f199e87f9f9e2
dominicbraam <dominic.braam@gmail.com>
2022-01-31 01:44:31 -0400

    Added configs for my laptop (commented out): Xorg, bspwm, polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,7 +4,11 @@ killall -q polybar && sleep 2
 
 echo "---" | tee -a /tmp/polybar1.log
 
+# Desktop
 polybar -r dominicbar 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar -r dominicbar-secondary 2>&1 | tee -a /tmp/polybar1.log & disown
+
+# Laptop
+# polybar -r dominicbar-laptop 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."

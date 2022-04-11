df254b7ff0d641cd284949162818cac939f0db12
dominicbraam <dominic.braam@gmail.com>
2022-04-11 19:59:31 -0400

    launch_at_start: initial commit for auto launching of apps on startup

---
+++ custom_scripts/launch_at_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+killall -qw discord
+
+# wait for an internet connection
+while ! wget http://google.com -O- 2>/dev/null | grep -q Lucky; do
+  sleep .1
+done
+
+# launch stuff
+discord &
+bspc rule -a Alacritty -o desktop='^2' follow=on && /usr/bin/alacritty -e btop &

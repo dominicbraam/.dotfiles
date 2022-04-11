59c6807d4969410f4c83552e0938886af2138ff7
dominicbraam <dominic.braam@gmail.com>
2022-04-11 16:49:07 -0400

    Xorg: started using scripts for different screen layouts

---
+++ Xorg/vert-4k_hori-1440.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --off --output DP-2 --primary --mode 2560x1440 --pos 2160x1099 --rotate normal --output DP-3 --mode 3840x2160 --scale 0.75x0.75 --pos 0x0 --rotate left --output HDMI-1 --off

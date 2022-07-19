2e4e9c321ec22c60e83506821048bbc132acfb5d
dominicbraam <dominic.braam@gmail.com>
2022-07-19 19:56:40 -0400

    Xorg: new layouts

---
+++ Xorg/hori_both1440.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --off --output DP-2 --primary --mode 2560x1440 --rate 120.02 --pos 0x319 --rotate normal --output DP-3 --mode 2560x1440 --pos 2560x319 --rotate normal --output HDMI-1 --off

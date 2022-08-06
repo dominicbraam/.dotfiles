6e57e65ee0cb847ad933d233140144094b9e032e
dominicbraam <dominic.braam@gmail.com>
2022-08-06 19:23:03 -0400

    custom: added config for work displays

--- custom_scripts/set_config.sh
+++ custom_scripts/set_config.sh
@@ -9,6 +9,14 @@ if [ $1 = '0' ]; then
   echo "polybar -r dominicbar-laptop 2>&1 | tee -a /tmp/polybar1.log & disown" > $HOME/.dotfiles/polybar/.config/polybar/device_config
   echo "[device]" > $HOME/.dotfiles/polybar/.config/polybar/device_vars
   echo "hwmon-path = /sys/devices/platform/coretemp.0/hwmon/hwmon5/device/hwmon/hwmon5/temp1_input" >> $HOME/.dotfiles/polybar/.config/polybar/device_vars
+elif [ $1 = '1' ]; then
+  echo "Xft.dpi: 150" > $HOME/.dotfiles/Xorg/.config/Xorg/.Xresources
+  echo "bspc monitor eDP1 -d 1 2 3 4 5" > $HOME/.dotfiles/bspwm/.config/bspwm/device_config
+  echo "bspc monitor HDMI3 -d 6 7 8 9 0" >> $HOME/.dotfiles/bspwm/.config/bspwm/device_config
+  echo "polybar -r dominicbar-laptop 2>&1 | tee -a /tmp/polybar1.log & disown" > $HOME/.dotfiles/polybar/.config/polybar/device_config
+  echo "polybar -r dominicbar-laptop-secondary 2>&1 | tee -a /tmp/polybar1.log & disown" >> $HOME/.dotfiles/polybar/.config/polybar/device_config
+  echo "[device]" > $HOME/.dotfiles/polybar/.config/polybar/device_vars
+  echo "hwmon-path = /sys/devices/platform/coretemp.0/hwmon/hwmon5/device/hwmon/hwmon5/temp1_input" >> $HOME/.dotfiles/polybar/.config/polybar/device_vars
 else
   echo "Xft.dpi: 100" > $HOME/.dotfiles/Xorg/.config/Xorg/.Xresources
   echo "bspc monitor DP-2 -d 1 2 3 4 5" > $HOME/.dotfiles/bspwm/.config/bspwm/device_config

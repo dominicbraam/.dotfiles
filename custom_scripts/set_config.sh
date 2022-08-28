7d0a7dbf47b899897c7309a7e30dda2ff5767bc0
dominicbraam <dominic.braam@gmail.com>
2022-08-28 12:40:51 -0400

    custom_script: updated cpu temp file for Macbook

--- custom_scripts/set_config.sh
+++ custom_scripts/set_config.sh
@@ -8,7 +8,7 @@ if [ $1 = '0' ]; then
   echo "bspc monitor eDP1 -d 1 2 3 4 5 6 7 8 9 0" > $HOME/.dotfiles/bspwm/.config/bspwm/device_config
   echo "polybar -r dominicbar-laptop 2>&1 | tee -a /tmp/polybar1.log & disown" > $HOME/.dotfiles/polybar/.config/polybar/device_config
   echo "[device]" > $HOME/.dotfiles/polybar/.config/polybar/device_vars
-  echo "hwmon-path = /sys/devices/platform/coretemp.0/hwmon/hwmon5/device/hwmon/hwmon5/temp1_input" >> $HOME/.dotfiles/polybar/.config/polybar/device_vars
+  echo "hwmon-path = /sys/devices/platform/coretemp.0/hwmon/hwmon3/device/hwmon/hwmon5/temp1_input" >> $HOME/.dotfiles/polybar/.config/polybar/device_vars
 elif [ $1 = '1' ]; then
   echo "Xft.dpi: 150" > $HOME/.dotfiles/Xorg/.config/Xorg/.Xresources
   echo "bspc monitor eDP1 -d 1 2 3 4 5" > $HOME/.dotfiles/bspwm/.config/bspwm/device_config
@@ -16,7 +16,7 @@ elif [ $1 = '1' ]; then
   echo "polybar -r dominicbar-laptop 2>&1 | tee -a /tmp/polybar1.log & disown" > $HOME/.dotfiles/polybar/.config/polybar/device_config
   echo "polybar -r dominicbar-laptop-secondary 2>&1 | tee -a /tmp/polybar1.log & disown" >> $HOME/.dotfiles/polybar/.config/polybar/device_config
   echo "[device]" > $HOME/.dotfiles/polybar/.config/polybar/device_vars
-  echo "hwmon-path = /sys/devices/platform/coretemp.0/hwmon/hwmon5/device/hwmon/hwmon5/temp1_input" >> $HOME/.dotfiles/polybar/.config/polybar/device_vars
+  echo "hwmon-path = /sys/devices/platform/coretemp.0/hwmon/hwmon3/device/hwmon/hwmon5/temp1_input" >> $HOME/.dotfiles/polybar/.config/polybar/device_vars
 else
   echo "Xft.dpi: 100" > $HOME/.dotfiles/Xorg/.config/Xorg/.Xresources
   echo "bspc monitor DP-2 -d 1 2 3 4 5" > $HOME/.dotfiles/bspwm/.config/bspwm/device_config

9c40ad066edc245a063c3e386b3b3bb419e38dc5
dominicbraam <dominic.braam@gmail.com>
2022-09-20 12:30:58 -0400

    custom: updated home assistant url; I finally put added it through the reverse proxy xD

--- custom_scripts/launch_at_start.sh
+++ custom_scripts/launch_at_start.sh
@@ -10,6 +10,6 @@ done
 # launch stuff
 # discord &
 mailspring &
-bspc rule -a Surf -o desktop='^10' state=tiled follow=off && /usr/bin/surf http://assistant.home:8123 &
+bspc rule -a Surf -o desktop='^10' state=tiled follow=off && /usr/bin/surf https://home.dominicbraam.com &
 bspc rule -a Surf -o desktop='^10' state=tiled follow=off && /usr/bin/surf https://pihole.local.dominicbraam.com &
 # bspc rule -a Alacritty -o desktop='^2' follow=on && /usr/bin/alacritty -e btop &

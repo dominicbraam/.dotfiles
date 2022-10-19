e2c15caef4c097ea06359b118907494bcbad48c0
dominicbraam <dominic.braam@gmail.com>
2022-10-18 22:51:29 -0400

    custom: corrected file for cpu temp

--- custom_scripts/set_config.sh
+++ custom_scripts/set_config.sh
@@ -8,7 +8,7 @@ if [ $1 = '0' ]; then
   echo "bspc monitor eDP1 -d 1 2 3 4 5 6 7 8 9 0" > $HOME/.dotfiles/bspwm/.config/bspwm/device_config
   echo "polybar -r dominicbar-laptop 2>&1 | tee -a /tmp/polybar1.log & disown" > $HOME/.dotfiles/polybar/.config/polybar/device_config
   echo "[device]" > $HOME/.dotfiles/polybar/.config/polybar/device_vars
-  echo "hwmon-path = /sys/devices/platform/coretemp.0/hwmon/hwmon3/device/hwmon/hwmon5/temp1_input" >> $HOME/.dotfiles/polybar/.config/polybar/device_vars
+  echo "hwmon-path = /sys/devices/platform/coretemp.0/hwmon/hwmon5/device/hwmon/hwmon5/temp1_input" >> $HOME/.dotfiles/polybar/.config/polybar/device_vars
 elif [ $1 = '1' ]; then
   echo "Xft.dpi: 150" > $HOME/.dotfiles/Xorg/.config/Xorg/.Xresources
   echo "bspc monitor eDP1 -d 1 2 3 4 5" > $HOME/.dotfiles/bspwm/.config/bspwm/device_config
@@ -26,3 +26,5 @@ else
   echo "[device]" > $HOME/.dotfiles/polybar/.config/polybar/device_vars
   echo "hwmon-path = /sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon3/temp1_input" >> $HOME/.dotfiles/polybar/.config/polybar/device_vars
 fi
+
+./../polybar/.config/polybar/launch.sh &

7bbd5093ba07984e78b478f8968275bcecfbb8cd
dominicbraam <dominic.braam@gmail.com>
2023-01-03 13:44:44 -0400

    custom: let all desktop apps start from this custom script rather than with bspwm directly

--- custom_scripts/launch_at_start.sh
+++ custom_scripts/launch_at_start.sh
@@ -2,14 +2,17 @@
 
 killall -qw discord
 
+nitrogen --restore &
+nm-applet &
+blueman-applet &
+
 # wait for an internet connection
 while ! wget http://google.com -O- 2>/dev/null | grep -q Lucky; do
   sleep .1
 done
 
 # launch stuff
-# discord &
-mailspring &
-bspc rule -a Surf -o desktop='^10' state=tiled follow=off && /usr/bin/surf https://home.dominicbraam.com &
-bspc rule -a Surf -o desktop='^10' state=tiled follow=off && /usr/bin/surf https://pihole.local.dominicbraam.com &
-# bspc rule -a Alacritty -o desktop='^2' follow=on && /usr/bin/alacritty -e btop &
+bspc rule -a discord desktop='^4' follow=off && discord &
+bspc rule -a Mailspring desktop='^5' follow=off && mailspring &
+# bspc rule -a Surf -o desktop='^10' state=tiled follow=off && /usr/bin/surf https://home.dominicbraam.com &
+# bspc rule -a Surf -o desktop='^10' state=tiled follow=off && /usr/bin/surf https://pihole.local.dominicbraam.com &

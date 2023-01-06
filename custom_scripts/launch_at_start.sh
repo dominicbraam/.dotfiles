fba5bbf4a66ae13a4d600d1d715c0c1bcbdc530d
Dominic Braam <dominic.braam@gmail.com>
2023-01-06 19:47:33 -0400

    custom: removed discord from startup

--- custom_scripts/launch_at_start.sh
+++ custom_scripts/launch_at_start.sh
@@ -12,7 +12,7 @@ while ! wget http://google.com -O- 2>/dev/null | grep -q Lucky; do
 done
 
 # launch stuff
-bspc rule -a discord desktop='^4' follow=off && discord &
+# bspc rule -a discord desktop='^4' follow=off && discord &
 bspc rule -a Mailspring desktop='^5' follow=off && mailspring &
 # bspc rule -a Surf -o desktop='^10' state=tiled follow=off && /usr/bin/surf https://home.dominicbraam.com &
 # bspc rule -a Surf -o desktop='^10' state=tiled follow=off && /usr/bin/surf https://pihole.local.dominicbraam.com &

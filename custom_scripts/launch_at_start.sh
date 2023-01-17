f5090e657b09664d3809c8b24610daf39b0790a5
Dominic Braam <dominic.braam@gmail.com>
2023-01-17 06:49:04 -0400

    custom: added custom scripts to change volume and notifications for volume battery status

--- custom_scripts/launch_at_start.sh
+++ custom_scripts/launch_at_start.sh
@@ -16,6 +16,6 @@ done
 
 # launch stuff
 # bspc rule -a discord desktop='^4' follow=off && discord &
-bspc rule -a Mailspring desktop='^5' follow=off && mailspring &
+bspc rule -a Mailspring desktop='^9' follow=off && mailspring &
 # bspc rule -a Surf -o desktop='^10' state=tiled follow=off && /usr/bin/surf https://home.dominicbraam.com &
 # bspc rule -a Surf -o desktop='^10' state=tiled follow=off && /usr/bin/surf https://pihole.local.dominicbraam.com &
--- zsh/.config/zsh/.zshrc
+++ zsh/.config/zsh/.zshrc
@@ -51,6 +51,7 @@ alias ll='exa --icons --group-directories-first -l -g --git'
 alias v='nvim'
 alias br="sudo $HOME/.dotfiles/custom_scripts/dp_brightness.sh"
 alias find='fd'
+alias grep='grep --colour=auto'
 alias mydot="cd $HOME/.dotfiles/"
 
 # $PATH

03b2d0686dd8ff6d2f49b3d721bd84bf9e8fe75a
Dominic Braam <dominic.braam@gmail.com>
2023-02-27 01:19:48 -0400

    custom, polybar: raise dpi and fonts sizes for laptop config

--- custom_scripts/set_config.sh
+++ custom_scripts/set_config.sh
@@ -4,7 +4,7 @@
 [[ -z "$1" ]] && { echo "Parameter is empty"; echo "(0 = laptop, 1 = laptop at work with their display, 2 = PC - dualscreen - hori-vert)" ; exit 1; }
 
 if [ $1 = '0' ]; then
-  echo "Xft.dpi: 100" > $HOME/.dotfiles/Xorg/.config/Xorg/.Xresources
+  echo "Xft.dpi: 120" > $HOME/.dotfiles/Xorg/.config/Xorg/.Xresources
 
   echo "bspc monitor eDP-1 -d 1 2 3 4 5 6 7 8 9 0" > $HOME/.dotfiles/bspwm/.config/bspwm/device_config
   chmod +x $HOME/.dotfiles/bspwm/.config/bspwm/device_config

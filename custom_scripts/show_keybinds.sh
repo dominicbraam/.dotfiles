8a23a85c732ac5b8acf1569d63c05aa5c9973c70
Dominic Braam <dominic.braam@gmail.com>
2023-03-31 01:52:00 -0400

    rofi: removed width flag from scripts using it

--- custom_scripts/show_keybinds.sh
+++ custom_scripts/show_keybinds.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat ~/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | rofi -dmenu -i -no-show-icons -width 1000
+cat ~/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | rofi -dmenu -i -no-show-icons

1decc9f3703815954f5148d087817a517982f7f5
dominicbraam <dominic.braam@gmail.com>
2024-05-22 15:33:08 -0400

    nvim: added custom terminal relative dimensions

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -11,6 +11,19 @@ M.ui = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
+
+  term = {
+    hl = "Normal:term,WinSeparator:WinSeparator",
+    sizes = { sp = 0.4, vsp = 0.4 },
+    float = {
+      relative = "editor",
+      row = 0.15,
+      col = 0.2,
+      width = 0.6,
+      height = 0.65,
+      border = "single",
+    },
+  },
 }
 
 return M

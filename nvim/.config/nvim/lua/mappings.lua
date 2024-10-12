0ab86ff707edd9dafe0db8f040b06d94538feed8
Dominic Braam <dominic.braam@gmail.com>
2024-10-12 22:23:54 +0200

    nvim: added transparency toggle

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -8,5 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>lg", ":LazyGit <cr>", { desc = "Open LazyGit"})
 map("n", "<leader>fg", ":Telescope live_grep <cr>", { desc = "Live Grep"})
+map("n", "<leader>tr", function() require('base46').toggle_transparency() end, { desc = "Toggle transparency" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

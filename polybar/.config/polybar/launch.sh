88312f64e1df68fd4c25074292b857c2c993fdbb
Dominic Braam <dominic.braam@gmail.com>
2024-10-17 21:52:26 +0200

    polybar: updated polybar config to support the tray module and corrected a few errors with colour selection

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-killall -q polybar && sleep 2
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar && sleep 2
 
 echo "---" | tee -a /tmp/polybar1.log
 
 source $HOME/.local/bin/bash-ini-parser/bash-ini-parser
-
 cfg_parser $HOME/.dotfiles/custom_config/device/displays
 cfg_section_display
 

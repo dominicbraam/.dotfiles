38e1a1e2dba2efcde46d7dc44f094198e9788179
Dominic Braam <dominic.braam@gmail.com>
2024-10-25 20:31:35 +0200

    chatgpt script: used formatter on file

--- scripts/rofi/chatgpt.sh
+++ scripts/rofi/chatgpt.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
 function run_chatgpt {
-  env OPENAI_KEY=$(secret-tool lookup apikey openai-shell-cli) alacritty --hold -e chatgpt --model gpt-4o-mini-2024-07-18 -p "$1" & sleep 0.25
+    env OPENAI_KEY="$(secret-tool lookup apikey openai-shell-cli)" alacritty --hold -e chatgpt --model gpt-4o-mini-2024-07-18 -p "$1" &
+    sleep 0.25
 
-  node_id=$(bspc query -N -n focused)
-  bspc node $node_id -t floating
-  bspc node $node_id -z top 0 -180
-  bspc node $node_id -z right 80 0
-  bspc node $node_id -z bottom 0 180
-  bspc node $node_id -z left -80 0
+    node_id=$(bspc query -N -n focused)
+    bspc node "$node_id" -t floating
+    bspc node "$node_id" -z top 0 -180
+    bspc node "$node_id" -z right 80 0
+    bspc node "$node_id" -z bottom 0 180
+    bspc node "$node_id" -z left -80 0
 }
 
 function chat_quick_response {
-  query=$(rofi -i -dmenu -no-show-icons -theme-str "listview { require-input: false; }" -p "🧠❓")
-  run_chatgpt "$query"
+    query=$(rofi -i -dmenu -no-show-icons -theme-str "listview { require-input: false; }" -p "🧠❓")
+    run_chatgpt "$query"
 }
 
 mode1="Quick Response"
 mode2="Conversation"
 mode3="History"
 options="$mode1\n$mode2\n$mode3"
-mode=$(echo -e $options | rofi -dmenu -no-show-icons -theme-str "listview { require-input: false; } inputbar { enabled: false; }")
+mode=$(echo -e "$options" | rofi -dmenu -no-show-icons -theme-str "listview { require-input: false; } inputbar { enabled: false; }")
 
 case $mode in
-  $mode1)
+"$mode1")
     chat_quick_response
     ;;
-  $mode2)
+"$mode2")
     run_chatgpt
     ;;
-  $mode3)
+"$mode3")
     run_chatgpt "history"
     ;;
-  *)
+*)
     exit
     ;;
 esac

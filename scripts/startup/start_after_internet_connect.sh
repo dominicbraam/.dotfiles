c473d3b6e8395283209bcf56a6869949b8b32bf5
Dominic Braam <dominic.braam@gmail.com>
2024-10-25 20:30:49 +0200

    startup_scipt: implemented a better way to query if device is connected to internet

--- scripts/startup/start_after_internet_connect.sh
+++ scripts/startup/start_after_internet_connect.sh
@@ -1,12 +1,12 @@
 # wait for an internet connection
-while ! wget http://google.com -O- 2>/dev/null | grep -q Lucky; do
-  sleep .1
+while ! wget -q --tries=10 --timeout=20 --spider -O - http://google.com >/dev/null; do
+    sleep .5
 done
 
 # launch stuff after internet connection
 
 # file sync app
-pgrep -x nextcloud > /dev/null || nextcloud &
+pgrep -x nextcloud >/dev/null || nextcloud &
 
 # bspc rule -a discord desktop='^4' follow=off && discord &
 pgrep -x Mailspring >/dev/null || mailspring --password-store="gnome-libsecret" &
@@ -14,4 +14,4 @@ pgrep -x gnome-calendar >/dev/null || gnome-calendar &
 pgrep -x planify >/dev/null || planify &
 
 # check for updates in .dotfiles
-$HOME/.dotfiles/cron/dotfiles/check_updates.sh
+"$HOME"/.dotfiles/cron/dotfiles/check_updates.sh

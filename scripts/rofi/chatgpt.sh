8e916618d2b59c10b4c8d01237c88c786874882e
Dominic Braam <dominic.braam@gmail.com>
2025-03-31 14:57:45 +0200

    chatgpt script: updated model

--- scripts/rofi/chatgpt.sh
+++ scripts/rofi/chatgpt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function run_chatgpt {
-    env OPENAI_KEY="$(secret-tool lookup apikey openai-shell-cli)" alacritty --hold -e chatgpt --model gpt-4o-mini-2024-07-18 -p "$1" &
+    env OPENAI_KEY="$(secret-tool lookup apikey openai-shell-cli)" alacritty --hold -e chatgpt --model gpt-4o -p "$1" &
     sleep 0.25
 
     node_id=$(bspc query -N -n focused)

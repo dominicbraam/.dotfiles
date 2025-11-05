6376a0048bedda17f8d6c4e4202c5b8b7d086fc1
Dominic Braam <dominic.braam@gmail.com>
2025-11-05 15:40:13 +0100

    nvim: updated formatting

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -1,38 +1,55 @@
 local options = {
-    formatters_by_ft = {
-        bash = { "shfmt" },
-        c = { "clang-format" },
-        css = { "prettier" },
-        html = { "prettier" },
-        javascript = { "prettier" },
-        json = { "prettier" },
-        lua = { "stylua" },
-        markdown = { "prettier" },
-        php = { "pretty-php" },
-        python = { "black" },
-        r = { "prettier" },
-        rust = { "rustfmt" },
-        sh = { "shfmt" },
-        sql = { "sql-formatter" },
-        toml = { "pyproject-fmt" },
-        typescript = { "prettier" },
-        yaml = { "prettier" },
-        zsh = { "shfmt" },
-    },
-    formatters = {
-        black = {
-            prepend_args = { "--line-length", "79" },
-        },
-        prettier = {
-            prepend_args = { "--tab-width", "4" },
-        },
-    },
+	formatters_by_ft = {
+		bash = { "shfmt" },
+		c = { "clang-format" },
+		css = { "prettier" },
+		html = { "prettier" },
+		javascript = { "prettier" },
+		javascriptreact = { "prettier" },
+		json = { "prettier" },
+		lua = { "stylua" },
+		markdown = { "prettier" },
+		php = { "pretty-php" },
+		python = { "black" },
+		r = { "prettier" },
+		rust = { "rustfmt" },
+		sh = { "shfmt" },
+		sql = { "sql-formatter" },
+		toml = { "pyproject-fmt" },
+		typescript = { "prettier" },
+		typescriptreact = { "prettier" },
+		yaml = { "prettier" },
+		zsh = { "shfmt" },
+	},
+	formatters = {
+		black = {
+			prepend_args = { "--line-length", "79" },
+		},
+		prettier = {
+			prepend_args = { "--tab-width", "4", "--print-width", "79" },
+		},
+		stylua = {
+			prepend_args = {
+				"--indent-type",
+				"Tabs",
+				"--indent-width",
+				"4",
+				"--column-width",
+				"79",
+			},
+		},
+		clang_format = {
+			prepend_args = {
+				"--style={BasedOnStyle: LLVM, IndentWidth: 4, TabWidth: 4, UseTab: Never, ColumnLimit: 79}",
+			},
+		},
+	},
 
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_fallback = false,
-    },
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = false,
+	},
 }
 
 require("conform").setup(options)
--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -9,3 +9,4 @@ local o = vim.o
 o.shiftwidth = 4
 o.tabstop = 4
 o.softtabstop = 4
+o.expandtab = false

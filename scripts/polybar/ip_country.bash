4b8a9b41a8ce1a38df880465f3469855bb0cee9f
Dominic Braam <dominic.braam@gmail.com>
2026-02-22 14:52:20 +0100

    custom: refactor script to get the country for current IP

--- scripts/polybar/ip_country.bash
+++ scripts/polybar/ip_country.bash
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
-PUBLIC_IP=$(curl -s https://api.ipify.org)
+# Cache last known IP to avoid redundant API calls
+# since ip monitor can output multiple lines and therefore,
+# trigger the api call mutliple times per change.
+last_ip=""
 
-COUNTRY_CODE=$(curl -s "https://ipapi.co/$PUBLIC_IP/country/")
+ip monitor address link route | while read -r _; do
+	# Get current public IP
+	current_ip=$(curl -s https://api.ipify.org)
 
-echo "$COUNTRY_CODE"
+	# Skip if empty (no internet) or unchanged.
+	# Helping solve the same problem like above:
+	# deduplicating since ip monitor hsa multi line output per process
+	if [[ -z "$current_ip" || "$current_ip" == "$last_ip" ]]; then
+		continue
+	fi
+
+	# Lookup country only when the IP changes
+	country=$(curl -s "https://ipapi.co/$current_ip/country/")
+
+	last_ip="$current_ip"
+
+	echo "$country"
+done
